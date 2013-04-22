3bbabd98379ddf20a11fd280a5a004e8b2ca5400
Avinash Padmanabhan <avinashpadmanabhan@gmail.com>
2013-04-22 12:00:30 -0700

    updating options parser

--- lib/acop.rb
+++ lib/acop.rb
@@ -1,35 +1,38 @@
 require 'optparse'
+require 'open-uri'
 
 module Acop
    def self.run(*args)
-      options = {}
+		options = {}
 
-      option_parser = OptionParser.new do |opts|
-         opts.banner = 'Usage: acop [URL]'
+		option_parser = OptionParser.new do |opts|
+			opts.banner = 'Usage: acop -u URL'
 
-         opts.on('-u', '--url URL', 'Url to be accessibility tested') do |tag|
-            options[:url] = tag
-         end
-      end
+			opts.on('-u', '--url URL', 'Url to be accessibility tested') do |tag|
+				options[:url] = tag
+			end
+		end
 
+		begin
+			option_parser.parse!
+			raise OptionParser::MissingArgument, "Url should be provided!" if options.keys.empty?
+			raise OptionParser::InvalidArgument, "Url cannot be resolved!" unless valid_url?(options[:url])
+		rescue OptionParser::ParseError => e
+			puts e.message
+			puts
+			puts option_parser
+			exit -1
+		end
+	end
+
+   def self.valid_url?(url)
       begin
-         option_parser.parse!
-         raise OptionParser::MissingArgument, "Url to be accessibility tested should be provided!" if options.keys.empty?
-         raise OptionParser::InvalidArgument, "Cannot resolve url!" unless valid_url(options[:url])?
-      rescue OptionParser::ParseError => e
-         puts e.message
-         puts
-         puts option_parser
-         exit -1
-      end
-   end
-
-   def valid_url?(url)
-      begin
+			url = "http://" + url unless url.include?("http")
          contents = open(url)
          return true
-      rescue
+      rescue Exception => e
          return false
       end
    end
+
 end

1bdbbce08699cc671932596a52d38af7ccceac9d
Avinash Padmanabhan <avinashpadmanabhan@gmail.com>
2013-04-23 21:10:22 -0700

    removing the external helper

--- lib/acop/accessibility.rb
+++ lib/acop/accessibility.rb
@@ -1,6 +1,5 @@
 require 'open-uri'
 require 'nokogiri'
-require_relative 'helper'
 
 module Acop
 	class Enforcer
@@ -28,7 +27,7 @@ module Acop
 			image_inputs   = input_elements.select {|image_input| image_input['type'] =~ /image/i}
 			error_messages = []
 			image_inputs.each do |input|
-				if (@ah.alt_empty_or_nil(input) and input.parent.name != "a")
+				if (@ah.attribute_empty_or_nil(input, "alt") and input.parent.name != "a")
 					error_messages.push("Missing alt text/attribute for image button with id/name: " + (input['name'] || input['id'] || ""))
 				end
 			end
@@ -39,7 +38,7 @@ module Acop
 			image_elements = source.css('img')
 			error_messages = []
 			image_elements.each do |element|
-				if (@ah.alt_empty_or_nil(element) and element.parent.name != "a")
+				if (@ah.attribute_empty_or_nil(element, "alt") and element.parent.name != "a")
 					error_messages.push("Missing alt text/attribute for image with src: " + element['src'])
 				end
 			end
@@ -78,8 +77,8 @@ module Acop
 	end
 
 	class Helpers
-		def alt_empty_or_nil(element)
-			if(element['alt'] == nil || element['alt'] == "")
+		def attribute_empty_or_nil(element, attribute)
+			if(element[attribute] == nil || element[attribute] == "")
 				return true
 			end
 			false
